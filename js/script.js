776d5e58b0b8ab813373d027234170686c80497a
edgarmartinez1212 <edgar.martinez9319@gmail.com>
2023-11-26 19:05:01 -0600

    added functionality

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let timesClickedNumEl = document.querySelector(".times-clicked-number");
+let clickedBtn = document.querySelector(".clicked-btn");
+let resetBtn = document.querySelector(".reset-btn");
+
+timesClickedNumEl.textContent = "0";
+
+function incrementByOne() {
+  let num = Number(timesClickedNumEl.textContent) + 1;
+  timesClickedNumEl.textContent = num;
+
+  if (num === 1000) {
+    setTimeout(function () {
+      alert("Congrats!");
+    }, 100);
+  }
+}
+
+function resetCounter() {
+  timesClickedNumEl.textContent = "0";
+}
+
+clickedBtn.addEventListener("click", incrementByOne);
+resetBtn.addEventListener("click", resetCounter);
